07e26fcf67327f0f38e18e7791f6129e8269559f
aprizalabyan <abi@jkt1.ebdesk.com>
2026-02-20 16:21:29 +0700

    setup tanstack/react-query for state-management

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,29 @@
 "use client";
 
 import * as React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import theme from "@/libs/theme"
+import theme from "@/libs/theme";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 2,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+          },
+        },
+      }),
+  );
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      {children}
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </ThemeProvider>
   );
 }
